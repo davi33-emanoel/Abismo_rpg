882889389a59bf9353bd7467ce64f1a23148c4d6
davi <emanoeldavi348@gmail.com>
2021-08-10 13:23:43 -0300

    Primeiro link separado da pagina incial, versao 0.03

---
+++ js/abismo.js
@@ -0,0 +1,9 @@
+function escolha (){
+    var escolha=prompt("ficar ou não?","Responda com sim ou nao em minusculo")
+    if (escolha === 'sim'){
+        location.replace('../../index.html')
+    }
+    else{
+            location.replace('../../index.html')
+    }
+}
