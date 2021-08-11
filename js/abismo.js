cbd11eda7b82fad588922e5b9d6c1edd83c24ffd
davi <emanoeldavi348@gmail.com>
2021-08-11 13:30:03 -0300

    Termino das fases dos personagens 0.08

--- js/abismo.js
+++ js/abismo.js
@@ -24,7 +24,7 @@ function escolha_bite3(){
         location.replace('../../index.html')
     }
     else{
-            location.replace('../../historia-personagens/bite/game_over.html')
+            location.replace('../../index.html')
     }
 }
 function escolha_bite(){
@@ -73,13 +73,71 @@ function escolha_josefina4(){
         location.replace('../../index.html')
     }
     else{
-            location.replace('../../historia-personagens/josefina/game-over.html')
+            location.replace('../../index.html')
     }
 }
 function escolha_josefina_game_over(){
     var escolha=prompt("Josefina não aguentou a pressâo de enfrentar seu medo, se quiser tentar novamente digite novamente, se quiser ir para o menu digite menu?","Responda novamente menu em minusculo")
     if (escolha === 'novamente'){
         location.replace('../../historia-personagens/josefina/fase1.html')
+    }
+    else{
+            location.replace('../../index.html')
+    }
+}
+// Fim fases JOSEFINA
+
+// Começo fases ORC MORSE
+
+function escolha_orc1(){
+    var escolha=prompt("enfrentar o medo que repentinamente apareçe ou voltar para a vila?","Responda com enfrentar ou voltar em minusculo")
+    if (escolha === 'enfrentar'){
+        location.replace('../../historia-personagens/orc/fase2.html')
+    }
+    else{
+            location.replace('../../historia-personagens/orc/game-over.html')
+    }
+}
+function escolha_orc2(){
+    var escolha=prompt("Ele engole seco, e pensa empurro ou vou embora?","Responda com empurrar ou voltar em minusculo")
+    if (escolha === 'empurrar'){
+        location.replace('../../historia-personagens/orc/fase3.html')
+    }
+    else{
+            location.replace('../../historia-personagens/orc/game-over.html')
+    }
+}
+function escolha_orc3(){
+    var escolha=prompt("Ele entrar ou deixar o medo tomar conta por completo?","Responda com entrar ou voltar em minusculo")
+    if (escolha === 'entrar'){
+        location.replace('../../historia-personagens/orc/fase4.html')
+    }
+    else{
+            location.replace('../../historia-personagens/orc/game-over.html')
+    }
+}
+function escolha_orc4(){
+    var escolha=prompt("Morse deve continuar para chegar ao final e encontrar o ponto de luz, ou voltar e desistir?","Responda com entrar ou voltar em minusculo")
+    if (escolha === 'continuar'){
+        location.replace('../../historia-personagens/orc/fase5.html')
+    }
+    else{
+            location.replace('../../historia-personagens/orc/game-over.html')
+    }
+}
+function escolha_orc5(){
+    var escolha=prompt("Morse agora pode enfrentar seus outros medos que surgirem em sua vida, se quiser voltar ao menu e ajudar aos outros personagens enfrentarem seus medos digite menu","Responda menu em minusculo")
+    if (escolha === 'menu'){
+        location.replace('../../index.html')
+    }
+    else{
+            location.replace('../../historia-personagens/orc/game-over.html')
+    }
+}
+function escolha_orc6(){
+    var escolha=prompt("Morse não superou seu medo voltou para sua vila, se quiser tentar novamente digite novamente ou se quiser ir para o menu digite menu","Responda novamente ou menu em minusculo")
+    if (escolha === 'novamente'){
+        location.replace('../../historia-personagens/orc/fase1.html')
     }
     else{
             location.replace('../../index.html')
